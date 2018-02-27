5883c1864bc6c9bac435cae5f3d98c82843188cc
Kammounji <36406776+Kammounji@users.noreply.github.com>
2018-02-28 00:37:27 +0100

    commit to comm

--- src/Encheres/GestionEncheresBundle/EncheresGestionEncheresBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Encheres\GestionEncheresBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class EncheresGestionEncheresBundle extends Bundle
-{
-}
--- src/Store/GestionProduitsBundle/StoreGestionProduitsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Store\GestionProduitsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StoreGestionProduitsBundle extends Bundle
-{
-}
